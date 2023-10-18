821acb45b88c784893808b428b2a0368ad79b5cf
Seffih Fadi <seffihfadi2004@gmail.com>
2023-10-18 09:56:16 +0100

    fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
+import User from '@models/User'
 import NextAuth from "next-auth/next"
 import GoogleProvider from 'next-auth/providers/google'
 import { connectToDB } from '@utils/database'
-import User from "@models/User"
 
 const handler = NextAuth({
   providers: [
